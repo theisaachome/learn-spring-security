3f485ef428ae5d4985213b892be5f1eeb19e0485
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-06-27 21:48:04 +0630

    rest project setup

---
+++ spring-basic-security/src/main/java/com/isaachome/controller/StudentController.java
@@ -0,0 +1,29 @@
+package com.isaachome.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.isaachome.model.Student;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+	private static final List<Student> STUDENTS = Arrays.asList(
+			new Student(1, "James Bond"),
+			new Student(2, "Maria Jones"),
+			new Student(3, "Anna Smith"));
+	@GetMapping("{studentID}")
+	public Student getStudent(@PathVariable("studentID")Integer studentID) {
+		return STUDENTS.stream()
+				.filter((student)->student.getStudentID()== studentID)
+				.findFirst()
+				.orElseThrow(()-> new IllegalStateException("Student " +
+				studentID + " does not exists."));
+	}
+}
