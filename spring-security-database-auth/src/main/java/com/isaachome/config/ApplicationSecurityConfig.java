cf166193fd23b27e17058005d2c3804115f9ce8a
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-07-13 13:53:12 +0630

    database authentication

---
+++ spring-security-database-auth/src/main/java/com/isaachome/config/ApplicationSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.isaachome.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig {
+
+	private final PasswordEncoder passwordEncoder;
+
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+//		.antMatchers("/api/**/").hasRole(AppUserRoles.STUDENT.name())
+//		.antMatchers(HttpMethod.GET,"/management/api/**").hasAuthority(AppUserPermission.COURSE_READ.getPermission())
+//		.antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(AppUserPermission.COURSE_READ.getPermission())
+//		.antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(AppUserPermission.COURSE_READ.getPermission())
+//		.antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(
+//				AppUserRoles.ADMIN.name(),
+//				AppUserRoles.ADMINTRAINEE.name())
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
+
+		return http.build();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails studentUser = User.builder()
+				.username("annasmith")
+				.password(passwordEncoder.encode("password"))
+//				.roles(AppUserRoles.STUDENT.name())// ROLE_STUDENT spring security will generate.
+				.authorities(AppUserRoles.STUDENT.getGrantedAuthorities())
+				.build();
+		var adminUser = User.builder().username("linda").password(passwordEncoder.encode("password"))
+//				.roles(AppUserRoles.ADMIN.name())
+				.authorities(AppUserRoles.ADMIN.getGrantedAuthorities())
+				.build();
+		var tomUser = User.builder().username("tom").password(passwordEncoder.encode("password"))
+//				.roles(AppUserRoles.ADMINTRAINEE.name())
+				.authorities(AppUserRoles.ADMINTRAINEE.getGrantedAuthorities())
+				.build();
+		return new InMemoryUserDetailsManager(studentUser, adminUser, tomUser);
+	}
+}
---
+++ spring-security-database-auth/src/main/java/com/isaachome/model/Student.java
@@ -0,0 +1,12 @@
+package com.isaachome.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+	private Integer studentID;
+	private String studentName;
+}
